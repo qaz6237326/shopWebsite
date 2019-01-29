7dd035672889d45f5868adb5f9283955e94e5a77
qinwl <qinwl@163.com>
2019-01-29 09:36:31 +0800

    first commit

---
+++ src/views/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div style="width:1200px;margin: 0 auto;">
+      <top></top>
+      <h1>首页</h1>
+    </div>
+</template>
+
+<script>
+import top from './components/top.vue'
+
+export default {
+  name: '',
+  components: {
+    top
+  },
+  data () {
+    return {}
+  },
+  created () {
+  },
+  methods: {},
+  mounted () {
+  }
+}
+</script>
